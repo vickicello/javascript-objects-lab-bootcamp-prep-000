72748346045e27ddfff660f2e789ce33d846bce1
Vicki Hamm <vicki.cello@gmail.com>
2018-04-10 23:58:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, obj, { [key]:value })
+  return Object.assign({}, obj, { [key]:value });
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
