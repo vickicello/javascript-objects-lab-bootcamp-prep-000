618b79c552c27997d8d9926d199a7842162f5e21
Vicki Hamm <vicki.cello@gmail.com>
2018-04-11 00:01:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  return 
+  object[key] = value;
+  return object;
+}
