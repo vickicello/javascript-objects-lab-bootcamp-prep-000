7ffd82a498e1e32fc101ca75d07f410bb8b74ae3
Vicki Hamm <vicki.cello@gmail.com>
2018-04-11 00:14:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
+function deleteFromObjectByKey(object, key) {
+      var obj = { prop: 1 };
+      updateObjectWithKeyAndValue(obj, 'prop2', 2);
+}
